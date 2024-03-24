ab3864e8396f919fe92dcedcd1d7a475313774b3
Sebastián Zavala Villagómez <you@example.com>
2024-03-24 22:30:41 +0000

    Nest home-manager config options

--- flake.nix
+++ flake.nix
@@ -7,9 +7,11 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Home manager
-    #home-manager.url = "github:nix-community/home-manager/release-23.05";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      #url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # TODO: Add any other flake you might need
     # hardware.url = "github:nixos/nixos-hardware";
