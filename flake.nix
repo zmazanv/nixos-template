b1ed41fbf43c01c73f4c500c32f1d34b33a58a86
Sebastián Zavala Villagómez <zmazanv@proton.me>
2024-04-09 14:12:57 -0400

    flake: Add username@hostname to `homeConfigurations`

--- flake.nix
+++ flake.nix
@@ -41,8 +41,8 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
-      # FIXME: replace with your username@hostname
-      "your-username@your-hostname" = home-manager.lib.homeManagerConfiguration {
+      # DONE: replace with your username@hostname
+      "sebas@obsidiana" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
         # > Our main home-manager configuration file <
